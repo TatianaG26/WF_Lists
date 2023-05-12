e06053d4405b4acab71e0b6eff53837b6a17d3ef
TayianaG26 <ttolpygo@gmail.com>
2023-05-12 20:10:38 +0300

    Добавьте файлы проекта.

---
+++ WF_Lists/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace WF_Lists
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_edit = new System.Windows.Forms.Button();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.tB_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tB_surname = new System.Windows.Forms.TextBox();
+            this.tB_mail = new System.Windows.Forms.TextBox();
+            this.tB_phone = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_Import = new System.Windows.Forms.Button();
+            this.btn_Export = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(19, 176);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(455, 148);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(221, 35);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(119, 23);
+            this.btn_add.TabIndex = 1;
+            this.btn_add.Text = "Додати";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.Location = new System.Drawing.Point(221, 65);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(119, 23);
+            this.btn_edit.TabIndex = 2;
+            this.btn_edit.Text = "Редагувати";
+            this.btn_edit.UseVisualStyleBackColor = true;
+            this.btn_edit.Click += new System.EventHandler(this.btn_edit_Click);
+            // 
+            // btn_del
+            // 
+            this.btn_del.Location = new System.Drawing.Point(221, 94);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(119, 23);
+            this.btn_del.TabIndex = 3;
+            this.btn_del.Text = "Видалити ";
+            this.btn_del.UseVisualStyleBackColor = true;
+            this.btn_del.Click += new System.EventHandler(this.btn_del_Click);
+            // 
+            // tB_name
+            // 
+            this.tB_name.Location = new System.Drawing.Point(93, 35);
+            this.tB_name.Name = "tB_name";
+            this.tB_name.Size = new System.Drawing.Size(100, 22);
+            this.tB_name.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ім\'я";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Прізвище";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "е-mail";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Телефон";
+            // 
+            // tB_surname
+            // 
+            this.tB_surname.Location = new System.Drawing.Point(93, 63);
+            this.tB_surname.Name = "tB_surname";
+            this.tB_surname.Size = new System.Drawing.Size(100, 22);
+            this.tB_surname.TabIndex = 9;
+            // 
+            // tB_mail
+            // 
+            this.tB_mail.Location = new System.Drawing.Point(93, 92);
+            this.tB_mail.Name = "tB_mail";
+            this.tB_mail.Size = new System.Drawing.Size(100, 22);
+            this.tB_mail.TabIndex = 10;
+            // 
+            // tB_phone
+            // 
+            this.tB_phone.Location = new System.Drawing.Point(92, 120);
+            this.tB_phone.Name = "tB_phone";
+            this.tB_phone.Size = new System.Drawing.Size(100, 22);
+            this.tB_phone.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 7);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Ведіть дані:";
+            // 
+            // btn_Import
+            // 
+            this.btn_Import.Location = new System.Drawing.Point(222, 124);
+            this.btn_Import.Name = "btn_Import";
+            this.btn_Import.Size = new System.Drawing.Size(119, 47);
+            this.btn_Import.TabIndex = 13;
+            this.btn_Import.Text = "Імпорт даних у файл";
+            this.btn_Import.UseVisualStyleBackColor = true;
+            this.btn_Import.Click += new System.EventHandler(this.btn_Import_Click);
+            // 
+            // btn_Export
+            // 
+            this.btn_Export.Location = new System.Drawing.Point(347, 124);
+            this.btn_Export.Name = "btn_Export";
+            this.btn_Export.Size = new System.Drawing.Size(127, 47);
+            this.btn_Export.TabIndex = 14;
+            this.btn_Export.Text = "Експорт даних з файлу";
+            this.btn_Export.UseVisualStyleBackColor = true;
+            this.btn_Export.Click += new System.EventHandler(this.btn_Export_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Очистити список";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 336);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Export);
+            this.Controls.Add(this.btn_Import);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tB_phone);
+            this.Controls.Add(this.tB_mail);
+            this.Controls.Add(this.tB_surname);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tB_name);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.btn_edit);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Анкета";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_edit;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.TextBox tB_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tB_surname;
+        private System.Windows.Forms.TextBox tB_mail;
+        private System.Windows.Forms.TextBox tB_phone;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_Import;
+        private System.Windows.Forms.Button btn_Export;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WF_Lists/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+/*Написать приложение – анкету, которую предлагается заполнить пользователю: имя, фамилия, e-mail, телефон. 
+После нажатия на кнопку «Добавить» Информация о пользователе попадает в ListBox, 
+в котором храниться информация о всех пользователях. 
+Также, в ListBox, по клику на строку с информацией о пользователе следует предусмотреть возможность 
+удаления пользователя из коллекции элементов ListBox, а также редактирования. 
+Редактирование информации о пользователе производиться путем подстановки данных из ListBox, 
+в соответствующие поля для ввода новой информации.
+Предусмотреть экспорт/импорт всей информации о пользователях в файл.*/
+namespace WF_Lists
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "Анкета";
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+        
+        public class Human
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public string Email { get; set; }
+            public string Phone { get; set; }    
+            public Human() { }
+            public Human(string name, string surname,string email, string phone)
+            {
+                Name = name;
+                Surname = surname;
+                Email = email;
+                Phone = phone;
+            }
+            public override string ToString()
+            {
+                return $"{Name} {Surname}, {Phone}, {Email}";
+            }
+        }
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            // если поля не пустые добавляем пользователя
+            if (!String.IsNullOrEmpty(tB_name.Text) && !String.IsNullOrEmpty(tB_surname.Text)
+                && !String.IsNullOrEmpty(tB_mail.Text) && !String.IsNullOrEmpty(tB_phone.Text))
+            {
+                Human h = new Human(tB_name.Text, tB_surname.Text, tB_mail.Text, tB_phone.Text);
+                listBox1.Items.Add(h);
+            }
+            else
+            {
+                MessageBox.Show("Заполните все поля", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            tB_name.Text = string.Empty;
+            tB_surname.Text = string.Empty;
+            tB_mail.Text = string.Empty;
+            tB_phone.Text = string.Empty;
+        }
+        private void btn_edit_Click(object sender, EventArgs e)
+        {            
+            //поменять полностью все данные
+            if (!String.IsNullOrEmpty(tB_name.Text) && !String.IsNullOrEmpty(tB_surname.Text)
+                && !String.IsNullOrEmpty(tB_mail.Text) && !String.IsNullOrEmpty(tB_phone.Text))
+            {
+                //есть выделенные
+                if (listBox1.SelectedItems != null)
+                {
+                    for (int i = 0; i < listBox1.SelectedItems.Count; i++)
+                    {
+                        listBox1.Items.Remove(listBox1.SelectedItems[i]);
+                        Human h = new Human(tB_name.Text, tB_surname.Text, tB_mail.Text, tB_phone.Text);
+                        listBox1.Items.Add(h);
+                    }
+                    tB_name.Text = string.Empty;
+                    tB_surname.Text = string.Empty;
+                    tB_mail.Text = string.Empty;
+                    tB_phone.Text = string.Empty;
+                }
+            }
+            else MessageBox.Show("Перед редагуванням заповніть поля даними\n" +
+                "виділіть поле яке хочете редагувати і натисніть \"Редагувати\"","Інструкція");
+        }
+        private void btn_del_Click(object sender, EventArgs e)
+        {
+            //список не пуст
+            if (listBox1.Items.Count != 0)
+            {
+                //есть выделенные
+                if (listBox1.SelectedItems != null)
+                {
+                    for (int i = 0; i < listBox1.SelectedItems.Count; i++)
+                    {
+                        listBox1.Items.Remove(listBox1.SelectedItems[i]);
+                    }
+                }
+            }            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Human h1 = new Human("Ірина", "Самойленко", "isam@gmail.com", "+380667894111");
+            Human h2 = new Human("Сергій", "Кравченко", "skrav@gmail.com", "+380931234567");
+            listBox1.Items.Add(h1);
+            listBox1.Items.Add(h2);
+        }
+        List<Human> list = new List<Human>();
+        private void btn_Import_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // нужно для сохранения данных в файл
+                foreach (var item in listBox1.Items)
+                {
+                    list.Add((Human)item);
+                }
+                //  Записываем данные в файл
+                using (StreamWriter writer = new StreamWriter("Humen.txt"))
+                {
+                    foreach (var user in list)
+                    {
+                        writer.WriteLine(user);
+                    }
+                }
+                MessageBox.Show("Дані завантажені у файл");
+            }catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        private void btn_Export_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Загрузить данные с файла
+                using (StreamWriter writer = new StreamWriter("Humen.txt"))
+                {
+                    foreach (Human user in list)
+                    {
+                        writer.WriteLine($"{user.Name},{user.Surname},{user.Email},{user.Phone}");
+                        listBox1.Items.Add(user.ToString());
+                    }
+                }
+                MessageBox.Show("Дані завантажені з файлу");
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
